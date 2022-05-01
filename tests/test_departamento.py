e739b96157744034389252569053a2b3ef65b6f8
Rodrigo Reis Ferreira <rodrigorf33@gmail.com>
2022-05-01 19:11:52 -0300

    Small fixes

--- tests/test_departamento.py
+++ tests/test_departamento.py
@@ -18,14 +18,13 @@ def test_load_lista_departamentos(load_lista_departamentos):
     lista_departamentos = load_lista_departamentos
     assert lista_departamentos is not None
     assert len(lista_departamentos) > 0
-    assert lista_departamentos[0].nome != ''
+    assert len(lista_departamentos[0].nome) > 0
     assert 'https://' in lista_departamentos[0].href
 
 def test_load_departamentos_count_nav(load_departamento_unico):
     browser = load_departamento_unico
     objRes = browser.xpath(cfg.queryXPath['RESULT_COUNT'])
     countItens = int(util.ExtractNumber(objRes[0].text_content().strip().replace('.', '')))
-
     assert len(objRes) > 0
     assert countItens > 0
 
